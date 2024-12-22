df2e7d4a20e1dfffebfff2cd719bc9eab7009366
daniele-aceti <daniele.aceti@hotmail.it>
2024-12-22 19:37:38 +0100

    feat: pari e dispari

--- js/main.js
+++ js/main.js
@@ -1,22 +1,28 @@
-    //RICHIESTA NUMERO
+//RICHIESTA NUMERO
+
+const controllerN = document.querySelector("#controllerN");
     
-    let num = prompt ("scrivi un numero da 1 a 5")
+let num = prompt ("scrivi un numero da 1 a 5")
     if(num <= 5){
-        console.log("il numero è corretto")
+        console.log(controllerN.innerHTML = "il numero è corretto")
     }else{
         num = false
-        console.log("il numero non è corretto")
+        console.log(controllerN.innerHTML = "il numero non è corretto")
     }
+//RICHIESTA PARI O DISPARI
 
-    //RICHIESTA PARI O DISPARI
+const controllerS = document.querySelector("#controllerS");
 
-   let condition = prompt ("inserisci pari o dispari")
+let condition = prompt ("inserisci pari o dispari")
+condition = condition.toLocaleLowerCase()
     if(condition === "pari" || condition === "dispari"){
-        console.log("il valore è valido")
+        console.log(controllerS.innerHTML = "il valore è valido")
     }else{
         condition = false
-        console.log("il valore non è valido")
+        console.log(controllerS.innerHTML = "il valore non è valido")
     }
+   
+
 
     //NUMERO RANDOM
 
@@ -25,13 +31,13 @@ function randomIntFromInterval (min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
   
-  const random = randomIntFromInterval(1, 5);
-  console.log(random)
+const random = randomIntFromInterval(1, 5);
+console.log(random)
   
 //FUNZIONE SOMMA
 
 function result (num1, num2) {
-    return num1 + num2;
+         return num1 + num2;
 }
 
 //SOMMA TRA RANDOM E NUMERO DIGITATO DALL'UTENTE
@@ -39,19 +45,30 @@ function result (num1, num2) {
 const sum = result(parseInt(num), random)
 console.log(sum)
 
-// PARI O DISPARI
+// PARI O DISPARI & VINCITORE 
+const winner = document.querySelector("#winner");
 
-function eveneOdd (){
+function evenOdd(even, odd){
     if(sum % 2 === 0){
-        console.log("pari")
+        const even = "pari"
+        console.log(even)
+        if(even === condition){
+            console.log(winner.innerHTML = "HAI VINTO")
+        }else{
+            console.log(winner.innerHTML = "HAI PERSO")
+        }
     }else if(sum % 2 === 1){
-        console.log("dispari")}
-    return
-}
+        const odd = "dispari"
+        console.log(odd)
+        if(odd === condition){
+            console.log(winner.innerHTML = "HAI VINTO")
+        }else{
+            console.log(winner.innerHTML = "HAI PERSO")
+        }
+        }
+           return 
+    }
 
-const winLose = eveneOdd()
-
-//VINCITORE
-
+const winLose = evenOdd()
 
 
