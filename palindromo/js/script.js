0994df0ca5bec493e56ef4dbb73ddd4585003d43
daniele-aceti <daniele.aceti@hotmail.it>
2024-12-23 12:36:08 +0100

    feat: note

--- palindromo/js/script.js
+++ palindromo/js/script.js
@@ -1,17 +1,28 @@
 const word = prompt("inserisci una parola")
+
+//trasformo la parola in un array per confrontarla con quella invertita
 let arrayWord = word.split("")
 console.log(arrayWord)
 arrayWord = arrayWord.toString()
 console.log(arrayWord)
+
+//dichiarazione e assegnazione della variabile content per usarla fuori della function
 let content = 0
+
+// funzione per invertire parola
    function reverseWord (word){
     content = word.split("")
     content = content.reverse()
     content = content.toString()
     console.log(content)
+    return
    }
+
+//uso della funzione per la parola inversa
   const reverse = reverseWord(word)
 
+  //if per verificare se la parola invertita e parola inserita sono uguali (palindrome)
+        
     if(content === arrayWord){
         console.log("è palindromo")
     }else{
