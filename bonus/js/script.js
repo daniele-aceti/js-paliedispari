ad1a98a1cb3bffe27aea1317099527ffb7aa3b08
daniele-aceti <daniele.aceti@hotmail.it>
2024-12-26 11:14:42 +0100

    feat: text

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -4,6 +4,8 @@ let condition = document.querySelector("#condition")
 let winner = document.querySelector("#winner")
 let pari = document.querySelector("#pari")
 let disapri = document.querySelector("#disapri")
+let systemNumber =  document.querySelector("#systemNumber")
+let add = document.querySelector('#sum')
 
 submitButton.addEventListener('click', function (event){
 
@@ -26,13 +28,13 @@ function randomIntFromInterval (min, max) {
   }
   
 const random = randomIntFromInterval(1, 5);
-console.log(random)
+systemNumber.innerHTML = `il mio numero è : ${random}`
 
 //SOMMA TRA RANDOM E NUMERO DIGITATO DALL'UTENTE
 
 
 const sum = parseInt(numbers.value) + parseInt(random)
-console.log(sum)
+add.innerHTML = `la somma dei nostri numeri è : ${sum}`
 
 
 
@@ -44,17 +46,17 @@ function evenOdd(even, odd){
         const even = pari.value
         console.log(even)
         if(even === condition.value){
-            console.log("HAI VINTO")
+            console.log(winner.innerHTML = "HAI VINTO")
         }else{
-            console.log("HAI PERSO")
+            console.log(winner.innerHTML = "HAI PERSO")
         }
     }else if(sum % 2 === 1){
         const odd = dispari.value
         console.log(odd)
         if(odd === condition.value){
-            console.log("HAI VINTO")
+            console.log(winner.innerHTML = "HAI VINTO")
         }else{
-            console.log("HAI PERSO")
+            console.log(winner.innerHTML = "HAI PERSO")
         }
         }
            return 
